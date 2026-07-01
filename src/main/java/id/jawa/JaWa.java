87daa634e62f9a0c1bca4c8ba830ea0990003d7d
jochris <jonathandev780@gmail.com>
2026-07-01 19:25:49 +0800

    build: update JaWa version string to match gradle build

--- src/main/java/id/jawa/JaWa.java
+++ src/main/java/id/jawa/JaWa.java
@@ -5,6 +5,6 @@ package id.jawa;
  * Placeholder root class. Public facade will be defined in id.jawa.core.JaWaClient.
  */
 public final class JaWa {
-    public static final String VERSION = "0.0.1-SNAPSHOT";
+    public static final String VERSION = "0.0.3-SNAPSHOT";
     private JaWa() {}
 }
